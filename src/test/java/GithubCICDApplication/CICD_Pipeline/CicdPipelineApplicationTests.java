6b73ea5045c37102ab2474ec97eafaf0e4fc049c
Emmanuel Sahr Sewah <e.sewah@peaqock.com>
2022-05-31 15:25:57 +0100

    initial commit

---
+++ src/test/java/GithubCICDApplication/CICD_Pipeline/CicdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package GithubCICDApplication.CICD_Pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
